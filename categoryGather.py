c6904c67ecbdc7f2e8295f4d44401b1140ba6113
SuperSweetJP <pokedatnis@gmail.com>
2020-10-08 20:40:38 +0300

    Deff: combined link and header logic into single func. started to build processLinksDb.

--- categoryGather.py
+++ categoryGather.py
@@ -4,19 +4,23 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
-db = mysql.connector.connect(
+mydb = mysql.connector.connect(
   host="localhost",
   user="root",
   password="anubissayshello",
   database="sslv"
 )
 
+mycursor = mydb.cursor()
+
 #hard coded category for now
 #ToDo rework into main loop that passes all categories later on
 categoryLink="https://www.ss.lv/lv/transport/cars/fiat/"
+catTitle = ""
 
 def scrapeListPage(pageLink):
     unique_link_set = list()
+    unique_header_set = list()
 
     source = requests.get(pageLink)
     soup = BeautifulSoup(source.content, "html.parser")
@@ -26,8 +30,17 @@ def scrapeListPage(pageLink):
             link = "http://www.ss.lv{}".format(link)
             if link not in unique_link_set:
                 unique_link_set.append(link)
+
+    for header in soup.find_all('a', class_="am"):
+        extract = header.text.splitlines()
+        extract2 = ''.join(extract)
+        if extract2 not in unique_header_set:
+            unique_header_set.append(extract2)
     
-    return unique_link_set
+    resultDict = {unique_link_set[i]: unique_header_set[i] for i in range(len(unique_link_set))}
+
+    return resultDict
+
 
 def categoryPageLoop(catLink):
     #screape the original page
@@ -42,13 +55,22 @@ def categoryPageLoop(catLink):
         processLinksDb(scrapeListPage(catLinkNextPage))
         i += 1
 
-#ToDo: add db logic here, add details gather call here
-def processLinksDb(linkList):
-    for link in linkList:
-        print(link)
-        #check if link already in db
-        #if not write new record
 
+#ToDo: add db logic here, add details gather call here
+def processLinksDb(linkDict):
+    for x, y in linkDict.items():
+        print(x, y)
+        #check if link already in db
+        sqlSelect = "SELECT link, Category from CarsTable WHERE link = %s AND Category = %s"
+        parm = (x, y)
+
+        mycursor.execute(sqlSelect, parm)
+        rows = mycursor.rowcount
+
+        print(rows)
+        #if  == 0:
+        #if not write new record
+            #print("record doesn't exist, inserting")
         #if yes, check if same header
 
             #if yes, update last seen
@@ -56,4 +78,5 @@ def processLinksDb(linkList):
             #if no, write new record
 
 
-#categoryPageLoop(categoryLink)
+categoryPageLoop(categoryLink)
+#print(scrapeListPage(categoryLink))
