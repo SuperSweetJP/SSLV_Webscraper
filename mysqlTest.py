db2089227fba7e11405018e5493dc6d8ac71697c
SuperSweetJP <ulmens@gmail.com>
2020-10-10 15:15:37 +0300

    Delete mysqlTest.py
    removing redundant file

--- mysqlTest.py
+++
@@ -1,6 +0,0 @@
-import datetime
-
-runDateTime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-print(runDateTime)
-#runDateTime = runDateTime.strftime('%Y-%m-%d %H:%M:%S')
-print(runDateTime)
